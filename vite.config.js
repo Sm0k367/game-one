9ec7b9074e5474747481896891b5a0c4bb51244f
Sm0k367 <139561920+Sm0k367@users.noreply.github.com>
2026-01-27 21:28:29 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    host: true,
-    port: 3000
-  },
   build: {
-    target: 'esnext', // Required for WebGPU top-level await
-    outDir: 'dist'
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'esnext'
-    }
+    target: 'esnext' // Important for WebGPU
   }
 });
